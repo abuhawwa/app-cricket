560950867c56520287cf194ad9263f7366b8ffd4
avinash.haneefa <avinash.haneefa@gmail.com>
2021-01-13 01:37:19 +0300

    Scorecard active tab

---
+++ src/store/modules/innings.js
@@ -0,0 +1,57 @@
+import * as firebase from "@/firebase";
+import router from "@/router";
+
+export default {
+  state: {
+    innings: [],
+  },
+  mutations: {
+    INNINGS(state, innings) {
+      state.innings = innings;
+    },
+  },
+  actions: {
+    async addMatch({ dispatch }, match) {
+      await firebase.matchesCollection
+        .add(match)
+        .then(() => {
+          dispatch("fetchMatch", match);
+          router.push({ name: "Play", params: { matchId: match.id } });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async fetchMatch({ commit }, match) {
+      await firebase.matchesCollection
+        .where("id", "==", parseInt(match.id))
+        .get()
+        .then((res) => {
+          res.forEach((item) => {
+            commit("INNINGS", item.data());
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async addIngsPlayers({ dispatch }, match) {
+      await firebase.matchesCollection
+        .where("id", "==", parseInt(match.id))
+        .get()
+        .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            firebase.matchesCollection.doc(doc.id).update(match);
+          });
+          dispatch("fetchMatch", match);
+          router.push({ name: "Scorecard", params: { match: match.id } });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  getters: {
+    innings: (state) => state.innings,
+  },
+};
---
+++ src/views/Play.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="card">
+    <h5 class="card-header">Add Players</h5>
+    <div class="card-body">
+      <form autocomplete="off" @submit.prevent="onPlay">
+        <div class="form-floating mb-3">
+          <input
+            type="text"
+            class="form-control"
+            id="striker"
+            placeholder="Striker Name"
+            v-model.trim="striker"
+          />
+          <label for="batsmenOne">Striker Name</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input
+            type="text"
+            class="form-control"
+            id="nonStriker"
+            placeholder="Non Striker Name"
+            v-model.trim="nonStriker"
+          />
+          <label for="nonStriker">Non Striker Name</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input
+            type="text"
+            class="form-control"
+            id="bowler"
+            placeholder="Bowler Name"
+            v-model.trim="bowler"
+          />
+          <label for="bowler">Bowler Name</label>
+        </div>
+        <button type="submit" class="btn btn-success d-block ms-auto">
+          Let's Play
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "Play",
+  data() {
+    return {
+      striker: "",
+      nonStriker: "",
+      bowler: "",
+    };
+  },
+  computed: {
+    ...mapGetters(["innings"]),
+  },
+  methods: {
+    onPlay() {
+      let innings = JSON.parse(JSON.stringify(this.innings));
+      const batsmans = [
+        { name: this.striker, isActive: true, id: 1 },
+        { name: this.nonStriker, isActive: false, id: 2 },
+      ];
+      const bowlers = [{ name: this.bowler, isActive: true, id: 1 }];
+      const ings = !innings.firstIngs.isEnd ? "firstIngs" : "secondIngs";
+      innings[ings].batsmans = batsmans;
+      innings[ings].bowlers = bowlers;
+      this.$store.dispatch("addIngsPlayers", innings);
+    },
+  },
+  created() {
+    let payload = {};
+    payload.id = this.$route.params.matchId;
+    this.$store.dispatch("fetchMatch", payload);
+  },
+};
+</script>
+
+<style>
+</style>
